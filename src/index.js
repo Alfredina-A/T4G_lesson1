fba4c6adb2719804ed7dd4661e3c62c434e9fb41
Alfredina-A <daldinart@gmail.com>
2024-11-25 14:12:24 +0000

    commiting all changes

---
+++ src/index.js
@@ -0,0 +1,35 @@
+alert ("hello world");
+
+// alert( "Dzagli Alfredina Abigail");
+// var x;
+// let x =5;
+// x=10
+// const y;
+
+
+//  let name = prompt("What is your name");
+//  alert (name);
+let friend = prompt("What's your friend's name?"); 
+let age = prompt("What's your friend's age");
+let favcolor = prompt ("What's your friend's favorite color");
+alert ("My friends name is" + friend + ". She is" + age + " years old and her favorite color is" + favcolor)
+alert (`My friends name is ${friend}. She is ${age} years and her favorite color is ${favcolor} ` )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
